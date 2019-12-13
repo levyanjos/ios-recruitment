ffa9e6f3988e23ff95467aece4fd605cd11efdb0
levycristian <jolevy.cristian@gmail.com>
2019-12-12 21:16:27 -0300

    View models created

---
+++ MovieShelf/View/Movies/Controllers/MovieCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCellViewModel.swift
+//  MovieShelf
+//
+//  Created by Levy Cristian on 12/12/19.
+//  Copyright © 2019 Levy Cristian. All rights reserved.
+//
+
+import Foundation
+
+class MovieCellViewModel {
+    
+}
---
+++ MovieShelf/View/Movies/Controllers/MoviesViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MoviesViewModel.swift
+//  MovieShelf
+//
+//  Created by Levy Cristian on 12/12/19.
+//  Copyright © 2019 Levy Cristian. All rights reserved.
+//
+
+import Foundation
+
+class MoviesViewModel {
+    
+}
